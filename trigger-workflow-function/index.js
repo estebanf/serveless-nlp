61babfa60ad984c78a0f9fb8383e217e2ffcdd86
Esteban Felipe <esteban@capbpm.com>
2021-02-22 19:04:34 -0500

    NLP Function

--- trigger-workflow-function/index.js
+++ trigger-workflow-function/index.js
@@ -1,6 +1,12 @@
 const {ExecutionsClient} = require('@google-cloud/workflows');
 const client = new ExecutionsClient();
 
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Creates a client
+const langClient = new language.LanguageServiceClient();
+
 const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
 const WORKFLOW_REGION = process.env.WORKFLOW_REGION;
 const WORKFLOW_NAME = process.env.WORKFLOW_NAME;
@@ -18,31 +24,47 @@ exports.trigger_workflow_function = async (event,context) => {
     const bucket = event.bucket; 
     const eventType = context.eventType;
 
-    console.log(`file ${file}`)
-    console.log(`bucket ${bucket}`)
-    console.log(`eventType ${eventType}`)
+    const document = {
+        gcsContentUri: `gs://${bucket}/${file}`,
+        type: 'PLAIN_TEXT',
+      };
 
-    // Let's just work if this is a new file
-    if (eventType == 'google.storage.object.finalize') {
-        // This is a new file, let's start the workflow
-        try {
-            const execResponse = await client.createExecution({
-                parent: client.workflowPath(GOOGLE_CLOUD_PROJECT,WORKFLOW_REGION, WORKFLOW_NAME),
-                execution: {
-                    argument: JSON.stringify({ file , bucket })
-                }
-            });
-            console.log(`Execution response: ${JSON.stringify(execResponse)}`);
+    const [result] = await client.analyzeEntities({document});
+    const entities = result.entities;
+    console.log('Entities:');
+    entities.forEach(entity => {
+      console.log(entity.name);
+      console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+      if (entity.metadata && entity.metadata.wikipedia_url) {
+        console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
+      }
+    });
+        
+    // console.log(`file ${file}`)
+    // console.log(`bucket ${bucket}`)
+    // console.log(`eventType ${eventType}`)
+
+    // // Let's just work if this is a new file
+    // if (eventType == 'google.storage.object.finalize') {
+    //     // This is a new file, let's start the workflow
+    //     try {
+    //         const execResponse = await client.createExecution({
+    //             parent: client.workflowPath(GOOGLE_CLOUD_PROJECT,WORKFLOW_REGION, WORKFLOW_NAME),
+    //             execution: {
+    //                 argument: JSON.stringify({ file , bucket })
+    //             }
+    //         });
+    //         console.log(`Execution response: ${JSON.stringify(execResponse)}`);
     
-            const execName = execResponse[0].name;
-            console.log(`Created execution: ${execName}`);
-            }
-        catch(e) {
-            console.error(`Error executing workflow: ${e}`);
-            throw e;            
-        }
-    }
-    else{
-        throw new Error(`Unexpected event type: ${eventType}`)
-    }
+    //         const execName = execResponse[0].name;
+    //         console.log(`Created execution: ${execName}`);
+    //         }
+    //     catch(e) {
+    //         console.error(`Error executing workflow: ${e}`);
+    //         throw e;            
+    //     }
+    // }
+    // else{
+    //     throw new Error(`Unexpected event type: ${eventType}`)
+    // }
 }
